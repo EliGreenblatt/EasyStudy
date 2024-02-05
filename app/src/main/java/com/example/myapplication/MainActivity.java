60b51cc103399c820368bd3cddb178bc9a2757c0
evyatar-yosef <evyataryoseff@gmail.com>
2024-02-05 13:42:21 +0200

    add eli

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Add a student's name to the "students" table
-        EasyStudy.addStudent("evyatar");
+        EasyStudy.addStudent("eli");
     }
 }
