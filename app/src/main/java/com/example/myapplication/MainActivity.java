e35ca07462371b5f316f4d3c0652c6a280cbfd2c
WildBoarStudio <96939859+WildBoarStudio@users.noreply.github.com>
2024-02-05 17:39:08 +0200

    Added Register

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,16 +5,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
-
+import android.widget.CheckBox;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity
 {
     private static final String TAG = "MainActivity";
-
+    CheckBox teacher;
     Button loginButton;
     Button registerButton;
     Button login;
+    Button register;
     TextInputLayout usernameInputLayout;
     TextInputLayout userPassInputLayout;
     @Override
@@ -22,12 +23,17 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        teacher = findViewById(R.id.checkBox); // Replace with your CheckBox ID
         login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
         loginButton = findViewById(R.id.loginButton); // Grab the login button reference
         registerButton = findViewById(R.id.registerButton); // Grab the register button reference
         usernameInputLayout = findViewById(R.id.UsernameText); // Grab the input username text
         userPassInputLayout = findViewById(R.id.PasswordText); // Grab the input user password text
 
+        teacher.setVisibility(View.GONE);
+        register.setVisibility(View.GONE);
         hideTextFields();
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,11 +60,25 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View v) {
                 // Handle Login button click
                 Log.i(TAG,"Register button clicked");
-                // Add your login logic or navigate to the login screen
+                hideButtons();
+                showTextFields();
+                loginButton.setVisibility(View.GONE);
+                login.setVisibility(View.GONE);
+                teacher.setVisibility(View.VISIBLE);
+                register.setVisibility(View.VISIBLE);
             }
         });
 
 
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle Login button click
+                Log.i(TAG,"Register button clicked second time");
+               // Add logic that registers do the database
+            }
+        });
+        
     }
 
     private void hideTextFields()
