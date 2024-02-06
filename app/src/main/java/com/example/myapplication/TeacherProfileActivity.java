0bd1b48da477e46b3f65ab8619ebed142df66c2f
WildBoarStudio <96939859+WildBoarStudio@users.noreply.github.com>
2024-02-06 21:15:23 +0200

    Added logout and update functions

--- app/src/main/java/com/example/myapplication/TeacherProfileActivity.java
+++ app/src/main/java/com/example/myapplication/TeacherProfileActivity.java
@@ -1,13 +1,50 @@
 package com.example.myapplication;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TeacherProfileActivity extends AppCompatActivity
 {
+    Button updateProfileButton;
+    Button logoutButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_profile);
+
+        updateProfileButton = findViewById(R.id.updateProfile);
+        logoutButton = findViewById(R.id.logout);
+        updateProfileButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // This code will execute when the updateProfileButton is clicked
+                Log.i("YourActivity", "updateProfileButton clicked");
+
+                // Add your logic to navigate to the update profile page for students
+                Intent intent = new Intent(TeacherProfileActivity.this, Update.class);
+                startActivity(intent);
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // This code will execute when the updateProfileButton is clicked
+                Log.i("YourActivity", "updateProfileButton clicked");
+
+                // Add your logic to navigate to the update profile page for students
+                Intent intent = new Intent(TeacherProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
