b26d2ab466307c79606929632dcfb443ee5b223b
Itamarc101 <itamarc101@gmail.com>
2024-02-07 17:36:24 +0200

    Added list of teachers

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
